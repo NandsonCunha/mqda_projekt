eb95b25cb572ae9992619c7f21da7399445bbef4
NandsonCunha <nandson.s.c@gmail.com>
2024-03-06 01:21:33 -0300

    Adicionando speedometer, teste

--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -16,61 +16,36 @@ export default function Notification(){
 
     return (
         <View style={styles.container}>
-        <Text style={styles.text}>Nível de Fumaça:</Text>
+            <View style={styles.containerMenor}>
+                <View style={styles.containerSel}>
+                    <Text style={styles.text}>Nível de Fumaça:</Text>
+                
+                    <SelectDropdown
+                        data={iqar}
+                        rowStyle={styles.selectDropdownRow}
+                        selectedRowStyle={styles.select}
+                        onSelect={(selectedItem, index) => {
+                            console.log(selectedItem, index)
+                        }}
+                        buttonTextAfterSelection={(selectedItem, index) => {
+                            return selectedItem
+                        }}
+                        rowTextForSelection={(item, index) => {
+                            return item
+                        }}
+                    />
+            </View>
+
+            <Switch
+                style={styles.switch}
+                value={isLigado}
+                onValueChange={handleToggleSwitch}
+                trackColor={{false: '#767577', true: '#767577'}}
+                thumbColor={isLigado ? '#25FE02' : '#FB0505'}
+            />
+            </View>
+            
         
-        <SelectDropdown
-            data={iqar}
-            rowStyle={{
-                borderColor: '#000'
-            }}
-            selectedRowStyle={{
-                borderColor:'#000'
-            }}
-            onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index)
-            }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem
-	        }}
-            rowTextForSelection={(item, index) => {
-                return item
-	        }}
-        />
-
-        <Switch
-            style={styles.switch}
-            value={isLigado}
-            onValueChange={handleToggleSwitch}
-        />
-        
-
-        
-        <Text style={styles.text}>Nível de Fumaça:</Text>
-
-        <SelectDropdown
-            data={iqar}
-            rowStyle={{
-                borderColor: '#000'
-            }}
-            selectedRowStyle={{
-                borderColor:'#000'
-            }}
-            onSelect={(selectedItem, index) => {
-                console.log(selectedItem, index)
-            }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem
-            }}
-            rowTextForSelection={(item, index) => {
-                return item
-            }}
-        />
-
-        <Switch
-            style={styles.switch}
-            value={isLigado}
-            onValueChange={handleToggleSwitch}
-        />
         </View>
     );
     };
@@ -78,17 +53,32 @@ export default function Notification(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "row",
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f1f1f1',
         backgroundColor: '#F0FFF8',
-        shadowColor: 'black'
+
+    },
+    containerMenor:{
+        flexDirection: "row",
+        height: 120,
+        width: 280,
+        padding: 15,
+        borderWidth: 0.1,
+        elevation:1,
     },
     text: {
         fontSize: 18,
         fontWeight: 'bold',
         marginBottom: 10,
     },
+    selectDropdownRow: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        borderTopColor: '#ccc',
+        borderTopWidth: 1,
+        borderRadius: 5,
+      },
     select: {
         width: 200,
         height: 40,
@@ -100,5 +90,8 @@ const styles = StyleSheet.create({
     switch: {
         marginLeft: 10,
     },
+    containerSel: {
+
+    }
     });
     
